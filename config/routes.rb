e6b97829a5269b052ae3aaf764c848458cd7a74e
thomasbirien <thomasbirien@gmail.com>
2020-02-05 17:29:32 +0100

    add validates, chang route cause bad redirection, change the method to call for generating reference

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "rides#index"
   resources :rides, only: :create
   patch "rides/start", to: "rides#start"
-  patch "rides/cancel", to: "rides#start"
+  patch "rides/cancel", to: "rides#cancel"
 end
